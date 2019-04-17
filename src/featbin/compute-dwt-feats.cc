f00cdf4b9dad2b36fd2fd207961e17ab03695aae
444ghost <444ghost.git@gmail.com>
2019-04-17 17:59:30 +0900

    compute-dwt-feats.cc

--- src/featbin/compute-dwt-feats.cc
+++ src/featbin/compute-dwt-feats.cc
@@ -27,26 +27,90 @@
 int main(int argc, char *argv[]) {
  
 	using namespace kaldi;
-	ParseOptions po("444ghost.ERROR: Wrong options");
+	ParseOptions po("444ghost.ERROR in compute-dwt-feats.cc: Invalid options for initializing ParseOptions ");
 	WaveletOptions wavelet_opts;
 
-	int32 num_feats;
-
 	wavelet_opts.Register(&po);
-
-	//po.Register("num-feats", &num_feats, "po.Register(\"num-feats\"...");
-
 	po.Read(argc, argv);
 
-	KALDI_LOG << "num_feats = " << wavelet_opts.num_feats;
-	KALDI_LOG << "wavelet_type = " << wavelet_opts.wavelet_type;
-	KALDI_LOG << "decomposition_level = " << wavelet_opts.decomposition_level;
+	KALDI_LOG << "444ghost.LOG in compute-dwt-feats.cc: num_feats = " << wavelet_opts.num_feats;
+	KALDI_LOG << "444ghost.LOG in compute-dwt-feats.cc: wavelet_type = " << wavelet_opts.wavelet_type;
+	KALDI_LOG << "444ghost.LOG in compute-dwt-feats.cc: decomposition_level = " << wavelet_opts.decomposition_level;
 
-	std::string hey = po.GetArg(1);
-	KALDI_LOG << hey;
+	// Define defaults for gobal options
+	std::string output_format = "kaldi";
+	int32 channel = -1; // -1 mono, 0 left, 1 right
+	BaseFloat min_duration = 0.0;
+	std::string wav_rspecifier = po.GetArg(1);
+	std::string output_wspecifier = po.GetArg(2);
 
 	Wavelet wavelet(wavelet_opts);
 
-	return -1;
+	SequentialTableReader<WaveHolder> reader(wav_rspecifier);
+    	BaseFloatMatrixWriter kaldi_writer;
+
+	if (output_format == "kaldi") {
+		if (!kaldi_writer.Open(output_wspecifier))
+			KALDI_ERR << "Could not initialize output with wspecifier " << output_wspecifier;
+	} else{
+
+		KALDI_ERR << "444ghost.ERROR in compute-dwt-feats.cc: output_format is not \"kaldi\"";
+	}
+	
+	int32 num_utts = 0, num_success = 0;
+
+	for (; !reader.Done(); reader.Next()) {
+		num_utts++;
+		std::string utt = reader.Key();
+		const WaveData &wave_data = reader.Value();
+		if (wave_data.Duration() < min_duration) {
+			KALDI_WARN << "File: " << utt << " is too short ("
+				   << wave_data.Duration() 
+				   << " sec): producing no output.";
+			continue;
+		}
+
+		int32 num_chan = wave_data.Data().NumRows(), this_chan = channel;
+		{  	// This block works out the channel (0=left, 1=right...)
+			KALDI_ASSERT(num_chan > 0);  // should have been caught in
+			// reading code if no channels.
+			if (channel == -1) {
+				this_chan = 0;
+				if (num_chan != 1)
+					KALDI_WARN << "Channel not specified but you have data with "
+				        	   << num_chan  
+						   << " channels; defaulting to zero";
+			} else {
+				if (this_chan >= num_chan) {
+					KALDI_WARN << "File with id " << utt << " has "
+				                   << num_chan 
+						   << " channels but you specified channel "
+				                   << channel 
+						   << ", producing no output.";
+		    			continue;
+				}
+			}
+		}
+
+		SubVector<BaseFloat> waveform(wave_data.Data(), this_chan);
+		Matrix<BaseFloat> features;
+		try {
+/********************************************************************************/
+			//mfcc.ComputeFeatures(waveform, wave_data.SampFreq(), vtln_warp_local, &features);
+/********************************************************************************/
+		} catch (...) {
+			KALDI_WARN << "Failed to compute features for utterance "
+			           << utt;
+			continue;
+		}
+
+		if (num_utts % 10 == 0)
+			KALDI_LOG << "Processed " << num_utts << " utterances";
+		KALDI_VLOG(2) << "Processed features for key " << utt;
+		num_success++;
+	}
+	KALDI_LOG << " Done " << num_success << " out of " << num_utts
+                  << " utterances.";
+	return (num_success != 0 ? 0 : 1);
 }
 
